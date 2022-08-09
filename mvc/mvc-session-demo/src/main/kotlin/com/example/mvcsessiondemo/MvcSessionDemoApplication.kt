5b3c88f4fde43cab32ab900073c4372bdbffa5af
JY8752 <j.y.8752@gmail.com>
2022-08-09 10:16:21 +0900

    mvc-session-demo作成

---
+++ mvc/mvc-session-demo/src/main/kotlin/com/example/mvcsessiondemo/MvcSessionDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mvcsessiondemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvcSessionDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MvcSessionDemoApplication>(*args)
+}
---
+++ mvc/mvc-session-demo/src/test/kotlin/com/example/mvcsessiondemo/MvcSessionDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mvcsessiondemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvcSessionDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
