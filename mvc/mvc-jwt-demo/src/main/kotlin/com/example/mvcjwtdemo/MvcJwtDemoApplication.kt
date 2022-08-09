1375e40be594a59c8a6330d71bce1a4aec2eb0b3
JY8752 <j.y.8752@gmail.com>
2022-08-10 08:28:27 +0900

    プロジェクト作成

---
+++ mvc/mvc-jwt-demo/src/main/kotlin/com/example/mvcjwtdemo/MvcJwtDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mvcjwtdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvcJwtDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MvcJwtDemoApplication>(*args)
+}
---
+++ mvc/mvc-jwt-demo/src/test/kotlin/com/example/mvcjwtdemo/MvcJwtDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mvcjwtdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvcJwtDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
